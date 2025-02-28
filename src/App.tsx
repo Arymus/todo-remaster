dd9d37bd8a81a967025d5b3c2624efa49ce96914
Arymus Reyes <arymusr@gmail.com>
2025-02-28 14:08:49 -0500

    Added task deletion

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,27 @@
+document.addEventListener("keydown", event => {
+  if (event.key === "Enter") {
+    addTask();
+  }
+})
+
+function deleteTask(tasks: HTMLElement, li: HTMLElement) {
+  const button: HTMLElement = document.createElement("button");
+  button.className = "bg-white rounded-[10px] p-1 ml-5 h-10 self-center hover:outline-white hover:outline-solid hover:outline-offset-2 active:outline-gray-500 active:outline-solid active:bg-gray-500"
+
+  button.innerHTML =
+  `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 24 24">
+  <path 
+  d="M 10 2 L 9 3 L 5 3 C 4.448 3 4 3.448 4 4 C 4 4.552 4.448 5 5 5 L 7 5 L 17 5 L 19 5 C 19.552 5 20 4.552 20 4 C 20 3.448 19.552 3 19 3 L 15 3 L 14 2 L 10 2 z M 5 7 L 5 20 C 5 21.105 5.895 22 7 22 L 17 22 C 18.105 22 19 21.105 19 20 L 19 7 L 5 7 z">
+  </path>
+  </svg>`;
+
+  tasks.append(button);
+  button.addEventListener("click", () => {
+    tasks.removeChild(li);
+    tasks.removeChild(button);
+  })
+}
+
 function addTask() {
   const giveTask = document.getElementById("giveTask")! as HTMLInputElement;
   const tasks: HTMLElement = document.getElementById("tasks")!;
@@ -8,9 +32,14 @@ function addTask() {
 
     const li: HTMLElement = document.createElement("li");
     li.innerText = giveTask.value.toString();
-    li.className = "text-white text-3xl font-bold"
+    li.className = "text-white text-3xl font-bold m-2"
     tasks.append(li);
+    giveTask.value = ""
     
+    for (const task in tasks_array) {
+      deleteTask(tasks, li);
+    }
+
     console.log(tasks_array);
 
   } else {
@@ -40,7 +69,7 @@ function App() {
         </form>
 
         <div className="flex flex-col items-center justify-center bg-rose-800 rounded-2xl p-10 m-20">
-          <ul id="tasks">
+          <ul id="tasks" className="flex flex-row">
 
           </ul>
         </div>
